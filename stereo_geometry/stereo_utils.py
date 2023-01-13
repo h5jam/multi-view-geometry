a5c1d446b99eb23c81d08c0cf632d123d824154f
h5jam <so.h4ns@gmail.com>
2023-01-13 20:10:59 +0900

    add: stereo geometry - fundamental matrix

--- stereo_geometry/stereo_utils.py
+++ stereo_geometry/stereo_utils.py
@@ -33,3 +33,60 @@ def plot_line(coeffs, xlim):
     x = np.linspace(xlim[0], xlim[1], 100)
     y = (a*x+c) / -b
     return x, y
+
+def compute_fundamental_matrix(points1, points2):
+    assert points1.shape[0] == points2.shape[0]  # check the number of points for correspondences
+
+    u1 = points1[:, 0]
+    v1 = points1[:, 1]
+    u2 = points2[:, 0]
+    v2 = points2[:, 1]
+    one = np.ones_like(u1)
+
+    A = np.c_[u1*u2, v1*u2, u2, u1*v2, v1*v2, v2, u1, v1, one]
+
+    U, S, V = np.linalg.svd(A, full_matrices=True)
+    f = V[-1, :]
+    F = f.reshape(3, 3)
+
+    U, S, V = np.linalg.svd(F, full_matrices=True)
+    S[-1] = 0
+    F = U @ np.diag(S) @ V
+
+    return F
+
+def compute_fundamental_matrix(points1, points2):
+    assert points1.shape[0] == points2.shape[0]  # check the number of points for correspondences
+
+    c1 = np.mean(points1, axis=0)
+    c2 = np.mean(points2, axis=0)
+
+    # scaling factor
+    s1 = np.sqrt(2 / np.mean(np.sum((points1 - c1) ** 2, axis=1)))
+    s2 = np.sqrt(2 / np.mean(np.sum((points2 - c2) ** 2, axis=1)))
+
+    # for cross product
+    T1 = np.array([
+        [s1, 0, -s1 * c1[0]],
+        [0, s1, -s1 * c1[1]],
+        [0, 0 ,1]
+    ])
+    T2 = np.array([
+        [s2, 0, -s2 * c2[0]],
+        [0, s2, -s2 * c2[1]],
+        [0, 0, 1]
+    ])  
+
+    normd_points1 = T1 @ points1.T
+    normd_points2 = T2 @ points2.T
+
+    # to resolve variance in the correspondences
+    normd_F = compute_fundamental_matrix(normd_points1.T, normd_points2.T)
+
+    return T2.T @ normd_F @ T1
+
+def compute_epipole(F):
+    U, S, V = np.linalg.svd(F)
+    e = V[-1, :]
+    e = e / e[2]
+    return e
